4fa041f5e063dc9c9e90198f9577fcbf0803fc76
Cirios Santhiago <cirios.santhiago@outlook.com>
2025-02-17 05:26:41 +0000

    NoLucky

---
+++ modules/programs/bash.nix
@@ -0,0 +1,12 @@
+{ delib, ... }:
+delib.module {
+  name = "programs.bash";
+
+  options = delib.singleEnableOption true;
+
+  home.ifEnabled.programs.bash = {
+    enable = true;
+
+    historyControl = [ "ignoredups" "ignorespace" ];
+  };
+}
